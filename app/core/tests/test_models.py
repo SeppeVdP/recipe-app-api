17f974c886b3db7987bbb3de0667a8ad7b13e937
Seppe Van de Poel <seppe.vandepoel@student.kdg.be>
2020-09-17 17:20:51 +0200

    admin added

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,16 +1,39 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successfull(self):
         """Test creating a new user with an email is successful"""
         email = 'seppe@hotmail.be'
         password = 'testpas'
-        user = get_user_model().objects.create.user(
+        user = get_user_model().objects.create_user(
             email=email,
             password=password
         )
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normailized(self):
+        """test the email for a new user is normailised"""
+        email = 'test@SEPPE.BE'
+        user = get_user_model().objects.create_user(email, 'test')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@email.be',
+            'test'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
